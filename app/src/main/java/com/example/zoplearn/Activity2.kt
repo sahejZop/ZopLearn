2830de953ed9bcc60248c06cf52e207bdfdb7eaa
Sahej Singh <sahej.singh@zopsmart.com>
2022-02-09 16:07:04 +0530

    basic android project

---
+++ app/src/main/java/com/example/zoplearn/Activity2.kt
@@ -0,0 +1,21 @@
+package com.example.zoplearn
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class Activity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+        Log.d("act2", "Act2 created")
+        /*
+        val intent = getIntent()
+
+
+         */
+    }
+}
